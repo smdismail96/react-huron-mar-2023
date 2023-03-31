b5db4c2739ddfd3614f5937051061ba13fcbd234
Prashanth Puranik <puranik@digdeeper.in>
2023-03-31 14:42:25 +0530

    Using Link, NavLink, and about SPA

--- projects/workshops-app/src/components/AppMenu/AppMenu.jsx
+++ projects/workshops-app/src/components/AppMenu/AppMenu.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const AppMenu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/">Workshops App</Navbar.Brand>
+                <Navbar.Brand to="/" as={NavLink} activeClassName="active">Workshops App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/workshops">Workshops List</Nav.Link>
-                        <Nav.Link href="/feedback">Feedback</Nav.Link>
+                        <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+                        <Nav.Link to="/workshops" as={NavLink}>Workshops List</Nav.Link>
+                        <Nav.Link to="/feedback" as={NavLink}>Feedback</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
