ccfcc3d031f11125080f32c43ed37c5c9e75955b
Prashanth Puranik <puranik@digdeeper.in>
2023-04-01 09:20:46 +0530

    using controlled components pattern

--- projects/workshops-app/src/components/AddSession/AddSession.uncontrolled.jsx
+++ projects/workshops-app/src/components/AddSession/AddSession.uncontrolled.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 
 import { postSession } from '../../services/sessions';
 
 const AddSession = () => {
-    // we shall "control" the inputs using these
-    const [sequenceId, setSequenceId] = useState('');
-    const [name, setName] = useState('');
-    const [speaker, setSpeaker] = useState('');
-    const [duration, setDuration] = useState('');
-    const [level, setLevel] = useState('');
-    const [abstract, setAbstract] = useState('');
+    const sequenceIdRef = useRef();
+    const nameRef = useRef();
+    const speakerRef = useRef();
+    const durationRef = useRef();
+    const levelRef = useRef();
+    const abstractRef = useRef();
 
     const { id } = useParams();
     const navigate = useNavigate();
@@ -20,17 +19,16 @@ const AddSession = () => {
         // Hey browser! please avoid doing anything
         event.preventDefault();
 
-        // // refObj.current is the reference to the underlying DOM node
+        // refObj.current is the reference to the underlying DOM node
         const session = {
             workshopId: +id,
             upvoteCount: 0,
-            // sequenceId: sequenceId
-            sequenceId: +sequenceId,
-            name,
-            speaker,
-            duration: +duration,
-            level,
-            abstract
+            sequenceId: sequenceIdRef.current.value,
+            name: nameRef.current.value,
+            speaker: speakerRef.current.value,
+            duration: durationRef.current.value,
+            level: levelRef.current.value,
+            abstract: abstractRef.current.value
         };
 
         console.log(session);
@@ -53,46 +51,27 @@ const AddSession = () => {
             <Form onSubmit={addSession}>
                 <Form.Group className="mb-3" controlId="sequenceId">
                     <Form.Label>Sequence ID</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={sequenceId}
-                        onChange={(event) => setSequenceId(event.target.value)}
-                    />
+                    <Form.Control type="text" ref={sequenceIdRef} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="name">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}
-                    />
+                    <Form.Control type="text" ref={nameRef} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="speaker">
                     <Form.Label>Speaker</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={speaker}
-                        onChange={(event) => setSpeaker(event.target.value)}
-                    />
+                    <Form.Control type="text" ref={speakerRef} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="duration">
                     <Form.Label>Duration</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={duration}
-                        onChange={(event) => setDuration(event.target.value)}
-                    />
+                    <Form.Control type="text" ref={durationRef} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="level">
                     <Form.Label>Level</Form.Label>
-                    <Form.Select
-                        value={level}
-                        onChange={(event) => setLevel(event.target.value)}
-                    >
+                    <Form.Select ref={levelRef}>
                         <option value="">-- Select level --</option>
                         <option value="Basic">Basic</option>
                         <option value="Intermdiate">Intermediate</option>
@@ -102,12 +81,7 @@ const AddSession = () => {
 
                 <Form.Group className="mb-3" controlId="abstract">
                     <Form.Label>Describe the session</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        type="text"
-                        value={abstract}
-                        onChange={(event) => setAbstract(event.target.value)}
-                    />
+                    <Form.Control as="textarea" type="text" ref={abstractRef} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
